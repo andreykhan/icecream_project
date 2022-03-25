6bd58d88f48ff7d8af35538d6d0ccac6137d3fe5
Andrey Khan <Andrey.Khan@kcell.kz>
2022-03-25 15:57:27 +0600

    routing urls & views

---
+++ anfisa/ice_cream/urls.py
@@ -0,0 +1,9 @@
+# ice_cream/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('ice_cream/', views.ice_cream_list),
+    path('ice_cream/<int:pk>/', views.ice_cream_detail),
+]

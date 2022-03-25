6e3bbaec66e486711b64a727d28e6b30123c0a32
Andrey Khan <Andrey.Khan@kcell.kz>
2022-03-25 17:42:52 +0600

    added urls and view-functions

--- anfisa/posts/views.py
+++ anfisa/posts/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse('main page')
+
+def group_posts(request, slug):
+    return HttpResponse('posts wwwwwwwwwwwwwwwgroup')

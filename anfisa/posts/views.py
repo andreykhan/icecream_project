52f8b93fb0c55fc47c72b1b44ade0ed4e64a6209
Andrey Khan <Andrey.Khan@kcell.kz>
2022-03-28 17:42:21 +0600

    django html, render

--- anfisa/posts/views.py
+++ anfisa/posts/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 
+from django.shortcuts import render
+
 def index(request):
-    return HttpResponse('main page')
+    template = 'templates/posts/index.html'
+    return render(request, template)
 
 def group_posts(request, slug):
     return HttpResponse('posts wwwwwwwwwwwwwwwgroup')

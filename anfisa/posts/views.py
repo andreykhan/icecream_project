d6a0172d6623351b0004854b2e07989ce940b755
Andrey Khan <Andrey.Khan@kcell.kz>
2022-03-28 17:53:33 +0600

    corrected posts.views

--- anfisa/posts/views.py
+++ anfisa/posts/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    template = 'templates/posts/index.html'
+    template = 'posts/index.html'
     return render(request, template)
 
 def group_posts(request, slug):

a2c27a851c4d3a2e457cea37ffa9f8bc175c0813
Andrey Khan <Andrey.Khan@kcell.kz>
2022-04-13 16:23:09 +0600

    added rendering

--- anfisa/posts/views.py
+++ anfisa/posts/views.py
@@ -4,7 +4,18 @@ from django.shortcuts import render
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    title = 'main page'
+    context = {
+        'title': title,
+        'text': 'это главная страница проекта anfisa'
+    }
+    return render(request, template, context)
 
 def group_posts(request, slug):
-    return HttpResponse('posts wwwwwwwwwwwwwwwgroup')
+    template = 'posts/group_list.html'
+    title = 'groups'
+    context = {
+        'title': title,
+        'text': 'Здесь будет информация о группах проекта Yatube'
+    }
+    return render(request, template, context)

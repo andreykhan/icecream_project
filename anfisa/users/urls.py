8f56ed93bbc45d8aab0eb7185fc2185498c6a91a
Andrey Khan <Andrey.Khan@kcell.kz>
2022-05-04 20:31:51 +0600

    added signup form

--- anfisa/users/urls.py
+++ anfisa/users/urls.py
@@ -6,4 +6,5 @@ app_name = 'users'
 
 urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='users/logged_out.html'), name='logout'),
+    path('signup/', views.SignUp.as_view(), name='signup')
 ]
--- anfisa/users/views.py
+++ anfisa/users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import CreateView # Импортируем CreateView, чтобы создать ему наследника
 
-# Create your views here.
+# Функция reverse_lazy позволяет получить URL по параметрам функции path()
+# Берём, тоже пригодится
+from django.urls import reverse_lazy
+
+from .forms import CreationForm # Импортируем класс формы, чтобы сослаться на неё во view-классе
+
+
+class SignUp(CreateView):
+    form_class = CreationForm
+    success_url = reverse_lazy('posts:index')  # После успешной регистрации перенаправляем пользователя на главную.
+    template_name = 'users/signup.html'
